9a3bce716879299d4b59cfac6b0c26156f223555
Ugnius <ugniusnaujokas@gmail.com>
2019-12-10 21:19:48 +0200

    Finished app10

--- App10/App10/Program.cs
+++ App10/App10/Program.cs
@@ -10,6 +10,10 @@ namespace App10
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(-6+3*5);
+            Console.WriteLine((13-2)*7);
+            Console.WriteLine((5 + -2) + (20/10));
+            Console.ReadLine();
         }
     }
 }
