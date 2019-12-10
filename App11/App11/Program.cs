eb7bde1b9aa9c215880b59e0da97bc6228c73f72
Ugnius <ugniusnaujokas@gmail.com>
2019-12-10 21:37:54 +0200

    End of app 10

---
+++ App11/App11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirmaji skaiciu");
+            String c = Console.ReadLine();
+            Console.WriteLine("Iveskite antraji skaiciu");
+            String d = Console.ReadLine();
+            double a = Convert.ToDouble(c);
+            double b = Convert.ToDouble(d);
+            Console.WriteLine(a * b) ;
+            Console.WriteLine(a / b);
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.ReadLine();
+        }
+    }
+}
