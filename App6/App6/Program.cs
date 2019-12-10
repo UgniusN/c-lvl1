06fb5445c9df46192d847108e5c4cd5e694d128b
Ugnius <ugniusnaujokas@gmail.com>
2019-12-10 21:07:46 +0200

    6th app

---
+++ App6/App6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 23;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
