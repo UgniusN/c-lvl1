77cc30ecd94eaa06e78fda52cb769ef11026fb46
Ugnius <ugniusnaujokas@gmail.com>
2019-12-10 20:05:10 +0200

    completed program

--- App3/App3/Program.cs
+++ App3/App3/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace App3
 {
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.GetArea();
+            r.Display();
+            Console.ReadLine();
+
         }
     }
 }
