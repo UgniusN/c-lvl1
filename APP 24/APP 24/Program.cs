35ada6b53dca6c643af4fada06a3c31234f395d5
Ugnius <ugniusnaujokas@gmail.com>
2019-12-16 01:09:05 +0200

    Completed app 25

--- APP 24/APP 24/Program.cs
+++ APP 24/APP 24/Program.cs
@@ -14,7 +14,7 @@ namespace APP_24
             String vardas = Console.ReadLine();
             Console.WriteLine("Iveskite pavarde");
             String pavarde = Console.ReadLine();
-            Console.WriteLine("{0}{1}",vardas,pavarde);
+            Console.WriteLine("{0} {1}",vardas,pavarde);
             Console.Read();
         }
     }
---
+++ ApP 25/ApP 25/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApP_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String miestas = "Panevezys";
+            String gatvesPav = "Naujamiescio";
+            int namoNr = 53;
+            int pastatymoMetai = 1989;
+            Console.WriteLine(miestas);
+            Console.WriteLine(gatvesPav);
+            Console.WriteLine(namoNr);
+            Console.WriteLine(pastatymoMetai);
+            int pradziosMetai = 1982;
+            Console.WriteLine(pradziosMetai);
+            int stateMetu = pastatymoMetai - pradziosMetai;
+            Console.WriteLine(stateMetu);
+            Console.ReadLine();
+
+        }
+    }
+}
