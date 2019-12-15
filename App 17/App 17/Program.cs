ae49759aa2f11d74476d584db6c0d6aa209f277b
Ugnius <ugniusnaujokas@gmail.com>
2019-12-15 21:57:45 +0200

    Completed App 17

---
+++ App 17/App 17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            String a = Console.ReadLine();
+            double b = Convert.ToDouble(a);
+            Console.WriteLine("{0} {0} {0} {0}", a);
+            Console.Read();
+        }
+    }
+}
