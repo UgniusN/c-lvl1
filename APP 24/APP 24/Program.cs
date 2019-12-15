4d26091e12628fb2f148b6a7c104d70286ded681
Ugnius <ugniusnaujokas@gmail.com>
2019-12-16 00:53:10 +0200

    Completed APP 24

--- APP 24/APP 24/Program.cs
+++ APP 24/APP 24/Program.cs
@@ -10,6 +10,12 @@ namespace APP_24
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite varda");
+            String vardas = Console.ReadLine();
+            Console.WriteLine("Iveskite pavarde");
+            String pavarde = Console.ReadLine();
+            Console.WriteLine("{0}{1}",vardas,pavarde);
+            Console.Read();
         }
     }
 }
