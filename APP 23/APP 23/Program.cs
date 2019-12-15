a43b78c9413218f2ac53b4604de74fda75471cc7
Ugnius <ugniusnaujokas@gmail.com>
2019-12-16 00:31:41 +0200

    Completed app 23

---
+++ APP 23/APP 23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            String a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            if(b >= 0)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.Read();
+        }
+    }
+}
