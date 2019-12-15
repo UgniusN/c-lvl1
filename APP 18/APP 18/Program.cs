97dbd1a2d096868159ebb4c3f1668a27de62d9b6
Ugnius <ugniusnaujokas@gmail.com>
2019-12-15 23:43:46 +0200

    End of APP 18

---
+++ APP 18/APP 18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 15;
+            int c;
+            c = b;
+            b = a;
+            a = c;
+        }
+    }
+}
