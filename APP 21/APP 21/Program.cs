e2ca9f40c25e68aba068ff5287d529650b22b764
Ugnius <ugniusnaujokas@gmail.com>
2019-12-16 00:02:08 +0200

    Completed APP 21

---
+++ APP 21/APP 21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite staciakampio ploti");
+            String a = Console.ReadLine();
+            int b = Convert.ToInt32(a);
+            Console.WriteLine("Iveskite staciakampio ilgi");
+            String c = Console.ReadLine();
+            int d = Convert.ToInt32(c);
+            Console.WriteLine(b * d);
+            Console.Read();
+        }
+    }
+}
