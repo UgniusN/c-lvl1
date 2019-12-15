9e918d0a9af2192f0ca2fe8fca204b688e907de4
Ugnius <ugniusnaujokas@gmail.com>
2019-12-15 23:52:10 +0200

    End of APP 19

---
+++ APP 19/APP 19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite pirma skaciu:");
+            String a = Console.ReadLine();
+            Console.WriteLine("Iveskite antra skaciu:");
+            String b = Console.ReadLine();
+            Console.WriteLine(a.Equals(b));
+            Console.Read();
+        }
+    }
+}
