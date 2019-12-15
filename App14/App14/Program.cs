2638253a7352cda320078b18fab10bdfd0c1fea2
Ugnius <ugniusnaujokas@gmail.com>
2019-12-15 11:15:31 +0200

    Completed app 14

--- App14/App14/Program.cs
+++ App14/App14/Program.cs
@@ -10,6 +10,17 @@ namespace App14
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite pirma skaiciu");
+            String c = Console.ReadLine();
+            Console.WriteLine("Iveskite antra skaiciu");
+            String d = Console.ReadLine();
+            double a = Convert.ToDouble(c);
+            double b = Convert.ToDouble(d);
+            Console.WriteLine(a+b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.Read();
         }
     }
 }
