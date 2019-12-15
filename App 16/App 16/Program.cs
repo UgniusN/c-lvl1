850fb00896bf91178a65a7d08507cbd861925fe7
Ugnius <ugniusnaujokas@gmail.com>
2019-12-15 19:54:07 +0200

    Started app 16

---
+++ App 16/App 16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String mokPavadinimas = "CodeAcademy";
+            String kursPavadinimas = "dotNet Kursai";
+            int studSkaicius = 20;
+            DateTime data = DateTime.Today;
+            Console.WriteLine(mokPavadinimas);
+            Console.WriteLine(kursPavadinimas);
+            Console.WriteLine(studSkaicius);
+            Console.Read();
+
+        }
+    }
+}
